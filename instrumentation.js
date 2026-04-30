9f2c02efde213b3273978ad5ac2e81cc2b6e3809
mew <mew.amornchairattanakul@datadoghq.com>
2026-05-01 03:17:54 +0800

    update sending logs via otlp

--- instrumentation.js
+++ instrumentation.js
@@ -2,23 +2,25 @@ const { NodeSDK } = require("@opentelemetry/sdk-node");
 const {
   WinstonInstrumentation,
 } = require("@opentelemetry/instrumentation-winston");
-
 const {
   OTLPTraceExporter,
 } = require("@opentelemetry/exporter-trace-otlp-grpc");
 const {
   OTLPMetricExporter,
 } = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const { OTLPLogExporter } = require("@opentelemetry/exporter-logs-otlp-grpc");
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
 const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+const { SimpleLogRecordProcessor } = require("@opentelemetry/sdk-logs");
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
   }),
+  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
   instrumentations: [
     getNodeAutoInstrumentations(),
     new WinstonInstrumentation(),
